4fe09e50b74267e6e98693de9b25f842ef543c12
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-09-14 21:20:00 +1000

    Removed Command class as it is redundant

--- java/src/main/java/com/example/suggest_dinner/Main.java
+++ java/src/main/java/com/example/suggest_dinner/Main.java
@@ -1,13 +1,17 @@
 package com.example.suggest_dinner;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public class Main {
 
     public static void main(String ...args) {
-        Recipe recipe = new Recipe("Beef Steak", Arrays.asList(new Ingredient("beef")));
-        Command command = new Command(Arrays.asList(recipe));
-        System.out.println(command.execute());
+        Ingredient beef = new Ingredient("beef");
+        Recipe beefSteak = new Recipe("Beef Steak", Arrays.asList(beef));
+        RecipePicker picker = new RecipePicker(Arrays.asList(beefSteak), Arrays.asList(beef));
+        Optional<Recipe> found = picker.pick();
+        String recipeName = found.isPresent() ? found.get().getName() : "";
+        System.out.println(recipeName);
     }
 
 }
