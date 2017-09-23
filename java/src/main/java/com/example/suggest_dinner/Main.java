05d44a382b3bca830076d36958d318802deb35f9
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-09-23 22:48:11 +1000

    Reliably read recipe file location passed as a command argument

--- java/src/main/java/com/example/suggest_dinner/Main.java
+++ java/src/main/java/com/example/suggest_dinner/Main.java
@@ -2,6 +2,7 @@ package com.example.suggest_dinner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.commons.cli.*;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,14 +12,27 @@ import java.util.Optional;
 public class Main {
 
     public static void main(String ...args) throws IOException {
-        File recipeFile = new File(args[1]);
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-        RecipeBook book = mapper.readValue(recipeFile, RecipeBook.class);
-        Ingredient beef = new Ingredient("beef");
-        RecipePicker picker = new RecipePicker(book, Arrays.asList(beef));
-        Optional<Recipe> found = picker.pick();
-        String recipeName = found.isPresent() ? found.get().getName() : "";
-        System.out.println(recipeName);
+        Options options = new Options();
+        options.addOption(Option.builder()
+                .longOpt("recipes")
+                .required()
+                .hasArg()
+                .build());
+        CommandLineParser parser = new DefaultParser();
+        try {
+            CommandLine cmdArgs = parser.parse(options, args);
+            File recipeFile = new File(cmdArgs.getOptionValue("recipes"));
+            ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+            RecipeBook book = mapper.readValue(recipeFile, RecipeBook.class);
+            Ingredient beef = new Ingredient("beef");
+            RecipePicker picker = new RecipePicker(book, Arrays.asList(beef));
+            Optional<Recipe> found = picker.pick();
+            String recipeName = found.isPresent() ? found.get().getName() : "";
+            System.out.println(recipeName);
+        } catch (ParseException e) {
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("suggest-dinner", options);
+        }
     }
 
 }
