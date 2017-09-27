a1aeeaf862fe8dded21481a3d02b1b876f886c28
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-09-27 21:48:28 +1000

    Test for different command version

--- test/test-command.js
+++ test/test-command.js
@@ -1,21 +1,6 @@
 
-const test = require('tape')
-const path = require('path')
-const childProcess = require('child_process')
-const commandPath = process.argv[2]
+const argv = require('minimist')(process.argv.slice(2))
+const commandVersion = argv['cmd-ver'] || '1';
+const versionTestDir = `./v${commandVersion}`;
 
-test('finds a single recipe', t => {
-  t.plan(1)
-
-  const command = `${commandPath} --stock ${__dirname}/test-data/v1/single-match-stock.yaml --recipes ${__dirname}/test-data/v1/single-match-recipes.yaml`
-  const output = childProcess.execSync(command, {encoding: 'utf8'})
-  t.deepEqual(output, 'Beef steak\n')
-})
-
-test('finds all recipes that needs can be cooked', t => {
-  t.plan(1)
-
-  const command = `${commandPath} --stock ${__dirname}/test-data/v1/multi-match-stock.yaml --recipes ${__dirname}/test-data/v1/multi-match-recipes.yaml`
-  const output = childProcess.execSync(command, {encoding: 'utf8'})
-  t.deepEqual(output, 'pot-au-feu\nBeef steak\n')
-})
+require(versionTestDir);
---
+++ test/v1/index.js
@@ -0,0 +1,21 @@
+
+const test = require('tape')
+const path = require('path')
+const childProcess = require('child_process')
+const commandPath = process.argv[2]
+
+test('finds a single recipe', t => {
+  t.plan(1)
+
+  const command = `${commandPath} --stock ${__dirname}/data/single-match-stock.yaml --recipes ${__dirname}/data/single-match-recipes.yaml`
+  const output = childProcess.execSync(command, {encoding: 'utf8'})
+  t.deepEqual(output, 'Beef steak\n')
+})
+
+test('finds all recipes that needs can be cooked', t => {
+  t.plan(1)
+
+  const command = `${commandPath} --stock ${__dirname}/data/multi-match-stock.yaml --recipes ${__dirname}/data/multi-match-recipes.yaml`
+  const output = childProcess.execSync(command, {encoding: 'utf8'})
+  t.deepEqual(output, 'pot-au-feu\nBeef steak\n')
+})
