e0538b32c5523ffa2faa75db52cbda108bd7d0e2
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-09-30 00:16:53 +1000

    Rename test data file

--- test/v1/index.js
+++ test/v1/index.js
@@ -7,7 +7,7 @@ const commandPath = process.argv[2]
 test('finds a single recipe', t => {
   t.plan(1)
 
-  const command = `${commandPath} --stock ${__dirname}/data/single-match-stock.yaml --recipes ${__dirname}/data/single-match-recipes.yaml`
+  const command = `${commandPath} --stock ${__dirname}/data/single-match--stock.yaml --recipes ${__dirname}/data/single-match--recipes.yaml`
   const output = childProcess.execSync(command, {encoding: 'utf8'})
   t.deepEqual(output, 'Beef steak\n')
 })
@@ -15,7 +15,7 @@ test('finds a single recipe', t => {
 test('finds all recipes that needs can be cooked', t => {
   t.plan(1)
 
-  const command = `${commandPath} --stock ${__dirname}/data/multi-match-stock.yaml --recipes ${__dirname}/data/multi-match-recipes.yaml`
+  const command = `${commandPath} --stock ${__dirname}/data/multi-match--stock.yaml --recipes ${__dirname}/data/multi-match--recipes.yaml`
   const output = childProcess.execSync(command, {encoding: 'utf8'})
   t.deepEqual(output, 'pot-au-feu\nBeef steak\n')
 })
